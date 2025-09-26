67ecc8d3b13268c86520da3e575b84fd32c21548
Tyrone Tong <tyrone.tong800@gmail.com>
2025-09-25 17:13:57 -0700

    fix: Correct CORS origin URL

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use((req, res, next) => {
 })
 
 app.use(cors({
-  origin: "https://full-stack-task-manager-app-omega.vercel.app/p" 
+  origin: "https://full-stack-task-manager-app-omega.vercel.app" 
 }));
 
 //routes
